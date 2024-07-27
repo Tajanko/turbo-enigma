65bb5a19e4b3623c67ab9cfd0162c88c0fddd141
Felix Tajanko <149209037+Tajanko@users.noreply.github.com>
2024-07-26 17:29:59 -0700

    Update SSD _Clinical.R

--- SSD _Clinical.R
+++ SSD _Clinical.R
@@ -24,7 +24,6 @@ original_names <- names(original_data)[-1:-6]
 modified_names <- make.names(original_names)
 name_mapping <- setNames(original_names, modified_names)
 
-
 # Read the data and rename columns
 data <- original_data %>%
   rename_all(make.names)
@@ -47,36 +46,43 @@ tidy_data$Observation <- factor(tidy_data$Observation)
 tidy_tukey<-tidy_data %>% 
   group_by(Observation) %>% 
   tukey_hsd(Values ~ Sex * Treatment) %>%
-  filter(term == "Sex:Treatment") %>%
   ungroup()
 
-ungroup(tidy_data)
-tidy_tukey <- tidy_tukey %>%
-  add_y_position(test = ., data = group_by(tidy_data, Observation), formula = Values ~ Sex:Treatment, fun = "max", scales = "free") %>%
-  add_x_position(test = ., x = 'Sex:Treatment')
+
+write.csv(tidy_tukey, "SSD_Clinical_TukeyHSD.csv", row.names = FALSE) 
+
+tidy_tukey<-tidy_tukey %>%
+  filter(term == "Sex:Treatment")
 
 # Filter to retain sets where at least one p-value is less than 0.05
-significant_observations <- tidy_tukey %>%
+sig_observations <- tidy_tukey %>%
   group_by(Observation) %>%
   filter(any(p.adj < 0.05)) %>%
-  filter(term == "Sex:Treatment") %>%
   ungroup()
 
-
 # Extract unique names of significant observations
-summary_table <- significant_observations %>%
+sig_observations <- sig_observations %>%
   distinct(Observation)
 
+write.csv(sig_observations, "SSD_Clinical_Sig_Observations.csv", row.names = FALSE)
 
 ############
-for (observation in summary_table$Observation) {
+for (observation in sig_observations$Observation) {
   original_observation <- name_mapping[[observation]]
   
   plot_data <- tidy_data %>%
     filter(Observation == observation) %>%
-    dplyr::select(Sex, Treatment, Values)
+    dplyr::select(Sex, Treatment, 'Sex:Treatment', Values)
   plot_tukey <- tidy_tukey %>%
-    filter(Observation == observation)
+    filter(Observation == observation) %>%
+    filter(p.adj.signif != 'ns') %>%
+    add_y_position(test = ., 
+                   data = plot_data, 
+                   formula = Values ~ Sex:Treatment, 
+                   fun = "max", 
+                   scales = "fixed", 
+                   step.increase = 0.12) %>%
+    add_x_position(test = ., x = 'Sex:Treatment')
   
   plot_tukey <- mutate(plot_tukey, xmin = case_when(xmin == 1 ~ 0.8,
                                                     xmin == 2 ~ 1.2,
@@ -84,15 +90,32 @@ for (observation in summary_table$Observation) {
   plot_tukey <- mutate(plot_tukey, xmax = case_when(xmax == 2 ~ 1.2,
                                                     xmax == 3 ~ 1.8,
                                                     xmax == 4 ~ 2.2))
-
-  p <- ggplot(plot_data, aes(x = Treatment, y = !!rlang::sym("Values"))) +
-    geom_boxplot(outlier.shape = NA, aes(fill = Sex), key_glyph = draw_key_rect) +  # Remove outliers from boxplot to avoid duplication with jitter
-    geom_point(aes(fill = Sex), size = 1.5, position=position_jitterdodge(jitter.width = 0.2), show.legend = F) +
+  
+  p <- ggplot(plot_data, 
+              aes(x = Treatment, y = !!rlang::sym("Values"))) +
+    geom_boxplot(outlier.shape = NA, 
+                 aes(fill = Sex), 
+                 key_glyph = draw_key_rect) +  # Remove outliers from boxplot to avoid duplication with jitter
+    geom_point(aes(fill = Sex), 
+               size = 1.5, 
+               position=position_jitterdodge(jitter.width = 0.2), 
+               show.legend = F) +
     labs(title = paste("Results for", original_observation),
          y = original_observation) +
-    stat_pvalue_manual(plot_tukey, label = "p.adj.signif", y.position = "y.position", xmin = "xmin", xmax = "xmax", hide.ns = TRUE, tip.length = 0)+
-    scale_fill_viridis(discrete = TRUE, alpha = 0.5, option="viridis") + theme_bw() +
-    theme(legend.key = element_rect(color = "black"), legend.key.spacing.y = unit(0.2, "cm"))
+    stat_pvalue_manual(plot_tukey, 
+                       label = "p.adj.signif", 
+                       y.position = "y.position", 
+                       xmin = "xmin", 
+                       xmax = "xmax", 
+                       hide.ns = TRUE, 
+                       tip.length = 0.02, 
+                       bracket.nudge.y = 0) +
+    scale_fill_viridis(discrete = TRUE, 
+                       alpha = 0.5, 
+                       option="viridis") + 
+    theme_bw() +
+    theme(legend.key = element_rect(color = "black"), 
+          legend.key.spacing.y = unit(0.2, "cm"))
   
   print(p)
   
