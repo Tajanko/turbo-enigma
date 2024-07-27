ea1f98ac664352b87e76aab5792eec20e59e1690
Felix Tajanko <149209037+Tajanko@users.noreply.github.com>
2024-07-26 17:29:01 -0700

    Update SSD _Clinical.R

--- SSD _Clinical.R
+++ SSD _Clinical.R
@@ -1,20 +1,15 @@
 # Load necessary libraries
 if(!require(readxl)) install.packages("readxl", dependencies=TRUE)
 if(!require(dplyr)) install.packages("dplyr", dependencies=TRUE)
-if(!require(tidyr)) install.packages("tidyr", dependencies=TRUE)
-if(!require(broom)) install.packages("broom", dependencies=TRUE)
-if(!require(purrr)) install.packages("purrr", dependencies=TRUE)
-if(!require(multcomp)) install.packages("multcomp", dependencies=TRUE)
 if(!require(ggplot2)) install.packages("ggplot2", dependencies=TRUE)
+if(!require(viridis)) install.packages("viridis", dependencies=TRUE)
 if(!require(rstatix)) install.packages("rstatix", dependencies=TRUE)
+
 
 library(readxl)
 library(dplyr)
-library(tidyr)
-library(purrr)
-library(broom)
-library(multcomp)
 library(ggplot2)
+library(viridis)
 library(rstatix)
 
 # Read the data and rename columns
@@ -23,25 +18,37 @@ data <- read_excel("SepticShockDataR.xlsx", sheet = "Clinical Data") %>%
 
 # Convert to tidy format
 tidy_data<-data %>% 
-  pivot_longer(cols = Mass.1..g.:SBC..mmol.L., names_to = "Observation", values_to = "Values")
+  pivot_longer(cols = Mass.1..g.:SBC..mmol.L., names_to = "Observation", values_to = "Values") %>%
+  mutate("Sex:Treatment" = paste0(Sex, ":", Treatment)) %>%
+  select(!c("Phase.1", "Phase.2")) %>%
+  drop_na(Values)
+
+tidy_data$`Sex:Treatment` <- factor(tidy_data$`Sex:Treatment`)
+tidy_data$Patient.ID <- factor(tidy_data$Patient.ID)
+tidy_data$Species <- factor(tidy_data$Species)
+tidy_data$Sex <- factor(tidy_data$Sex)
+tidy_data$Treatment <- factor(tidy_data$Treatment)
+tidy_data$Observation <- factor(tidy_data$Observation)
+
+# Read the summary table of unique significant observations
+#tidy_tukey <- read.csv("SSD_Clinical_tukey_sig_results2.csv")
 
 # Perform Tukey's HSD test
 tidy_tukey<-tidy_data %>% 
   group_by(Observation) %>% 
-  tukey_hsd(Values ~ Sex * Treatment)
+  tukey_hsd(Values ~ Sex * Treatment) %>%
+  filter(term == "Sex:Treatment") %>%
+  ungroup()
 
 # Filter to retain sets where at least one p-value is less than 0.05
 significant_observations <- tidy_tukey %>%
   group_by(Observation) %>%
   filter(any(p.adj < 0.05)) %>%
+  filter(term == "Sex:Treatment") %>%
   ungroup()
 
-# Extract unique names of significant observations
-unique_significant_observations <- significant_observations %>%
-  distinct(Observation)
+ungroup(tidy_data)
+tidy_tukey <- tidy_tukey %>%
+  # add_y_position(test = ., data = tidy_data, formula = Values ~ group, fun = "max")
+add_y_position(test = ., data = group_by(tidy_data, Observation), formula = Values ~ Sex:Treatment, fun = "max")
 
-# Save the summary table to a CSV file
-write.csv(unique_significant_observations, "SSD_Clinical_sig_observations2.csv", row.names = FALSE)
-
-# Save significant Tukey results to a CSV file
-write.csv(significant_observations, "SSD_Clinical_tukey_sig_results2.csv", row.names = FALSE)
